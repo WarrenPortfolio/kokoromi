5243c591ce5ec03dd19469d900c4b21663a12046
WarrenPortfolio <42305148+WarrenPortfolio@users.noreply.github.com>
2021-12-21 18:26:44 -0800

    Example Application

---
+++ Projects/ExampleApplication/Source/ExampleApplication.cpp
@@ -0,0 +1,33 @@
+//
+// Defines the entry point for the application.
+//
+
+// Including SDKDDKVer.h defines the highest available Windows platform.
+// If you wish to build your application for a previous Windows platform, include WinSDKVer.h and
+// set the _WIN32_WINNT macro to the platform you wish to support before including SDKDDKVer.h.
+#include <SDKDDKVer.h>
+
+// Windows Header Files
+#include <windows.h>
+
+// C RunTime Header Files
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+int APIENTRY wWinMain(
+	_In_		HINSTANCE	hInstance,
+	_In_opt_	HINSTANCE	hPrevInstance,
+	_In_		LPWSTR		lpCmdLine,
+	_In_		int			nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hInstance);
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+	UNREFERENCED_PARAMETER(nCmdShow);
+
+	// TODO: Place code here.
+
+	return ERROR_SUCCESS;
+}
